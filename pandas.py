4f4b1232576bf2c6da6fe546279bf58278039698
Bright Kumedzro <kumedzrobright@gmail.com>
2024-07-27 14:39:35 +0000

    Getting started with Pandas library

---
+++ pandas.py
@@ -0,0 +1,3 @@
+#Pandas library is useful for Data Processing & Analysis
+#The library has two obejects: Panda Data Frame and Pandas Series
+#Pandas Data Frame is a two-dimensional tabular data structure with labelled axes(rows and columns)
