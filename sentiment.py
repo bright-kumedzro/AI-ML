164726a285c2258d705d36198dabfd39316379d8
Bright Kumedzro <kumedzrobright@gmail.com>
2024-07-27 23:11:45 +0000

    Dataset addition

--- sentiment.py
+++ sentiment.py
@@ -36,3 +36,43 @@ def compute_metrics(eval_pred):
 
 from huggingface_hub import notebook_login
 notebook_login()
+
+
+from transformers import TrainingArguments, Trainer
+ 
+repo_name = "finetuning-sentiment-model-3000-samples"
+ 
+training_args = TrainingArguments(
+   output_dir=repo_name,
+   learning_rate=2e-5,
+   per_device_train_batch_size=16,
+   per_device_eval_batch_size=16,
+   num_train_epochs=2,
+   weight_decay=0.01,
+   save_strategy="epoch",
+   push_to_hub=True,
+)
+ 
+trainer = Trainer(
+   model=model,
+   args=training_args,
+   train_dataset=tokenized_train,
+   eval_dataset=tokenized_test,
+   tokenizer=tokenizer,
+   data_collator=data_collator,
+   compute_metrics=compute_metrics,
+)
+
+trainer.train()
+
+trainer.evaluate()
+
+trainer.push_to_hub()
+
+from transformers import pipeline
+ 
+sentiment_model = pipeline(model="federicopascual/finetuning-sentiment-model-3000-samples")
+sentiment_model(["I love this move", "This movie sucks!"])
+
+[{'label': 'LABEL_1', 'score': 0.9558},
+ {'label': 'LABEL_0', 'score': 0.9413}]
